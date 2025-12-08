1d630137dcb3b1da63d583cc434cf34380d6699a
Petr Fifka <petr.fifka@tredgate.cz>
2025-12-08 17:06:37 +0100

    Add data driven tests, using .env, .env example, dictionary for storing and reusing apps texts

---
+++ assets/dictionaries/dictionary.ts
@@ -0,0 +1,14 @@
+// assets/dictionaries/
+// dictionary.ts
+export const pmtoolTexts = {
+  login: {
+    title: "Login",
+    usernamePlaceholder: "Username",
+    loginButton: "Login",
+    lostPassword: "Password forgotten?",
+    rememberMeLabel: "Remember Me",
+  },
+  dashboard: {},
+  projects: {},
+  general: {},
+};
--- tests/learning/fluent-interface/fluent_login.spec.ts
+++ tests/learning/fluent-interface/fluent_login.spec.ts
@@ -15,32 +15,3 @@ test("Login Test Using Fluent Interface", async ({ page }) => {
     .then((dashboard) => dashboard.clickProfile())
     .then((dashboard) => dashboard.clickLogout());
 });
-/*
-Cvičení - vytvoření testu na ztrátu hesla pomocí Fluent API (⌛10:00)
-Vytvořte nový test na ztrátu hesla (tests/exercises/fluent_lost_password_tests.spec.ts)
-Zmigrujte Page Object pro stránku ztráta hesla (lost_password_page.ts)  do fluent interface ve složce: src/pages/pmtool
-Vytvořte novou akci v Lost Password Page třídě:
-Kliknutí na tlačítko zpět: clickBack(), 
-lokátor: #back-btn
-Přidejte return hodnoty do všech metod.
-Vytvořte testy:
-Ztracené heslo end to end (username: lost_password_user, email: lost_password@tredgate.cz)
-Otevření stránky ztraceného hesla, návrat na login.
-
- Názvy testů vymyslete sami
-
- 
-Výzva
-Vytvořte nový test v Automation Test Store: change_profile_tests.spec.ts, ve složce: tests/challenges
-Založte si účet (manuálně) pomocí například mailu například z https://temp-mail.org/ 
-Vytvořte test: Challenge Fluent: Change Telephone Test
-Kroky v testu:
-Přihlaste se
-Otevřete: Edit account details
-Změňte telefonní číslo
-Odhlašte se
-Využijte Page Objecty s Fluent API
-
-
-
-*/
